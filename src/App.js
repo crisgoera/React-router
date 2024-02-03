b6e610b3e8762bea95bb20cc3f7bd6ae3510a999
Cristian Goenaga <crisgoera@gmail.com>
2024-02-03 18:12:29 +0100

    Implemented axios for an API JSON simulation

--- src/App.js
+++ src/App.js
@@ -9,48 +9,38 @@ import { Routes, Route, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import Footer from './Footer';
 import { format } from 'date-fns'
+import api from './api/posts'
 
 function App() {
     const [postBody, setPostBody] = useState('')
     const [postTitle, setPostTitle] = useState('')
     const [search, setSearch] = useState('');
-    const [posts, setPosts] = useState([
-        {
-          id: 1,
-          title: "My First Post",
-          datetime: "July 01, 2021 11:17:36 AM",
-          body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-        },
-        {
-          id: 2,
-          title: "My 2nd Post",
-          datetime: "July 01, 2021 11:17:36 AM",
-          body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-        },
-        {
-          id: 3,
-          title: "My 3rd Post",
-          datetime: "July 01, 2021 11:17:36 AM",
-          body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-        },
-        {
-          id: 4,
-          title: "My Fourth Post",
-          datetime: "July 01, 2021 11:17:36 AM",
-          body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-        },
-        {
-            id: 5,
-            title: "My Test Post",
-            datetime: "July 01, 2021 11:17:36 AM",
-            body: "Test text for the filter function"
-          }
-      ]);
+    const [posts, setPosts] = useState([]);
     const [filteredPosts, setFilteredPosts] = useState(null);
 
     const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const response = await api.get('/posts')
+                setPosts(response.data)
+            } catch (err) {
+                if (err.response) {
+                // Not in the 200 response range:
+                console.log(err.response.data);
+                console.log(err.response.headers);
+                console.log(err.response.status);
+                } else {
+                    console.log(`Error: ${err.message}`)
+                }
+            }
+        }
+
+        fetchPosts();
+    }, [])
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
         const id = posts.length ? posts[posts.length-1].id +1 : 1;
         const datetime = format(new Date(), 'MMMM dd, yyyy pp');
@@ -60,16 +50,27 @@ function App() {
             body: postBody,
             datetime
         };
-        const postsList = [...posts, newPost];
-        setPosts(postsList);
-        setPostTitle('');
-        setPostBody('');
-        navigate('/');
+
+        try {
+            const response = await api.post('/posts', newPost)
+            const postsList = [...posts, response.data];
+            setPosts(postsList);
+            setPostTitle('');
+            setPostBody('');
+            navigate('/');
+        } catch (err) {
+            console.log(`Error: ${err.message}`)
+        }
     }
 
-    const handleDelete = (id) => {
-        setPosts(posts.filter((post) => post.id !== id));
-        navigate('/');
+    const handleDelete = async (id) => {
+        try{
+            await api.delete(`/posts/${id}`)
+            setPosts(posts.filter((post) => post.id !== id));
+            navigate('/');
+        } catch (err) {
+            console.log(`Error: ${err.message}`)
+        }
     }
 
     const handleSearch = (search) => {
